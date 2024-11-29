7787216b38e6a20f67739461aa37f3837f563059
maruf2509 <223012412@eastdelta.edu.bd>
2024-11-30 01:34:24 +0600

    add new variable

--- db.php
+++ db.php
@@ -3,6 +3,7 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "user_management";
+$cookies= "local";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
